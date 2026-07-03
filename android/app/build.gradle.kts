683e6a90d99a074956803e01febceb50987c5544
CodeKiteshub <ak1648475@gmail.com>
2026-07-03 18:29:12 +0530

    Update iOS project settings and dependencies
    - Increased MinimumOSVersion in AppFrameworkInfo.plist from 13.0 to 15.0.
    - Updated project.pbxproj to include new Pods and frameworks, and adjusted build phases for better integration.
    - Added new xcconfig files for Pods and updated build configurations for Runner and RunnerTests targets.
    - Updated pubspec.yaml and pubspec.lock to reflect new versions of Firebase and Cloud Firestore packages.
    - Added Podfile.lock to manage CocoaPods dependencies.
    - Introduced Package.resolved for Swift Package Manager dependencies.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,9 +1,18 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
+}
+
+val keystoreProperties = Properties().apply {
+    val keystorePropertiesFile = rootProject.file("key.properties")
+    if (keystorePropertiesFile.exists()) {
+        load(keystorePropertiesFile.inputStream())
+    }
 }
 
 android {
@@ -31,11 +40,18 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
